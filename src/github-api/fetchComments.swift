406426a4617ff6ddd419d2f3fa0fee40eaaa4b9b
diegolavalledev <58344228+diegolavalledev@users.noreply.github.com>
2021-11-10 15:13:06 +0000

    Using checked continuation on Linux. Fetching discussions asynchronously. Attempt 3.

--- src/github-api/fetchComments.swift
+++ src/github-api/fetchComments.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+#if canImport(_Concurrency)
+    import _Concurrency
+#endif
+
 #if canImport(FoundationNetworking)
     import FoundationNetworking
 #endif
@@ -15,9 +19,25 @@ func fetchComments() async -> [Discussion] {
     request.httpMethod = "POST"
     request.httpBody = GraphQLRequest.createDiscussionsRequest().asJSON
 
-    guard let (data, _) = try? await URLSession.shared.data(for: request),
-          let result = try? JSONDecoder().decode(DiscussionsResponse.self, from: data) else {
+    #if canImport(FoundationNetworking)
+        let data: Data = await withCheckedContinuation { continuation in
+            URLSession.shared.dataTask(with: request) { data, _, _ in
+                guard let data = data else {
+                    fatalError()
+                }
+                continuation.resume(returning: data)
+            }.resume()
+        }
+    #else
+        guard let response = try? await URLSession.shared.data(for: request) else {
+            fatalError()
+        }
+        let data = response.0
+    #endif
+
+    guard let result = try? JSONDecoder().decode(DiscussionsResponse.self, from: data) else {
         fatalError()
     }
+
     return result.data.repository.discussions.edges.map(\.node).map(Discussion.init)
 }
